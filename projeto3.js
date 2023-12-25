8b442b9cab77f83a3fec4a3f93c9f287bf67c80a
giosantini <giovannisantini98@gmail.com>
2023-12-25 13:04:09 -0300

    segundo commit testando

---
+++ projeto3.js
@@ -0,0 +1,33 @@
+class personagem{
+    constructor(nome, idade, tipo){
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+}
+
+atacar(){
+    let ataque;
+
+    switch (this.tipo){
+        case 'mago':
+        ataque = 'usou magia';
+        break;
+
+        case 'guerreiro':
+        ataque = 'usou espada';
+        break
+
+        case 'monge':
+        ataque = 'usou artes marciais'
+        break
+        
+        case 'ninja':
+        ataque = 'usou shuriken'
+        break
+
+        default:
+        ataque ='usou ataque genérico'
+    }
+    console.log(`O ${this.tipo} ${this.nome} atacou usando ${ataque}`)
+}
