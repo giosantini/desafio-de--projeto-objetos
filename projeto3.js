7834b974e8ddca037a63816d1d04b49b725591ba
giosantini <giovannisantini98@gmail.com>
2023-12-25 13:07:41 -0300

    testando

--- projeto3.js
+++ projeto3.js
@@ -4,30 +4,30 @@ class personagem{
         this.idade = idade;
         this.tipo = tipo;
     }
-}
-
-atacar(){
-    let ataque;
-
-    switch (this.tipo){
-        case 'mago':
-        ataque = 'usou magia';
-        break;
-
-        case 'guerreiro':
-        ataque = 'usou espada';
-        break
-
-        case 'monge':
-        ataque = 'usou artes marciais'
-        break
-        
-        case 'ninja':
-        ataque = 'usou shuriken'
-        break
-
-        default:
-        ataque ='usou ataque genérico'
+    atacar(){
+        let ataque;
+    
+        switch (this.tipo){
+            case 'mago':
+            ataque = 'usou magia';
+            break;
+    
+            case 'guerreiro':
+            ataque = 'usou espada';
+            break
+    
+            case 'monge':
+            ataque = 'usou artes marciais'
+            break
+            
+            case 'ninja':
+            ataque = 'usou shuriken'
+            break
+    
+            default:
+            ataque ='usou ataque genérico'
+        }
+        console.log(`O ${this.tipo} ${this.nome} atacou usando ${ataque}`)
     }
-    console.log(`O ${this.tipo} ${this.nome} atacou usando ${ataque}`)
 }
+
